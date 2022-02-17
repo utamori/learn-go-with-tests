e49c78b2c145486c407b7d35d58b6077fabf2312
utamori <mori_yuta@retail-ai.jp>
2022-02-17 12:39:35 +0900

    update hello-world sample code

--- hello-world/v8/hello_test.go
+++ hello-world/v8/hello_test.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestHello(t *testing.T) {
 
-	assertCorrectMessage := func(t *testing.T, got, want string) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
 		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
